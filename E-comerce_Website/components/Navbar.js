c67922ef00cfed2bb4f96f26a278942d4d61e7a3
3811 Raj Solanki <solankiraj9642@gmail.com>
2024-09-03 12:35:38 +0530

    e-commerce

--- E-comerce_Website/components/Navbar.js
+++ E-comerce_Website/components/Navbar.js
@@ -1,14 +1,15 @@
 const Navbar = () => {
-  let isLogin = localStorage.getItem('isLogin') || false
-  let username = localStorage.getItem('username')
-  return `    
-  <nav class="navbar navbar-expand-lg navbar-light bg-dark my-3">
+  let isLogin = localStorage.getItem('isLogin') || false;
+  let username = localStorage.getItem('username');
+  
+  return `
+  <nav class="navbar navbar-expand-lg navbar-light my-3" style="box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
     <div class="container">
       <a class="navbar-brand" href="#">
         <img src="https://1000logos.net/wp-content/uploads/2021/02/Shopee-logo.png" alt="Logo" height="50">
-      </a>      
+      </a>
       <div class="collapse navbar-collapse justify-content-between" id="navbarNav">
-        <ul class="navbar-nav ">
+        <ul class="navbar-nav mr-auto">
           <li class="nav-item mx-3">
             <a class="nav-link active text-danger px-3 fw-bolder" aria-current="page" href="/E-comerce_Website/index.html">Home</a>
           </li>
@@ -16,25 +17,28 @@ const Navbar = () => {
             <a class="nav-link text-danger px-3 fw-bolder" href="/E-comerce_Website/Pages/productPage.html">Product</a>
           </li>
           <li class="nav-item mx-3">
-            <a class="nav-link active text-danger px-3 fw-bolder" aria-current="page" href="/E-comerce_Website/Pages/addProductPage.html">Add Product</a>
+            <a class="nav-link text-danger px-3 fw-bolder" aria-current="page" href="/E-comerce_Website/Pages/addProductPage.html">Add Product</a>
           </li>
           <li class="nav-item mx-3">
             <a class="nav-link text-danger px-3 fw-bolder" href="/E-comerce_Website/Pages/cartPage.html">Cart</a>
           </li>
+        </ul>
+        <ul class="navbar-nav ml-auto">
           <li class="nav-item mx-3">
-            <a class="nav-link text-danger px-3 fw-bolder" href="/E-comerce_Website/Pages/loginPage.html"> ${isLogin ? "Logout" : "Login"}</a>
+            <a class="nav-link text-danger px-3 fw-bolder " href="/E-comerce_Website/Pages/loginPage.html">
+              ${isLogin ? "Logout" : "Login"}
+            </a>
           </li>
           <li class="nav-item mx-3">
-            <a class="nav-link text-danger px-3 fw-bolder" href="/E-comerce_Website/Pages/signUpPage.html">${isLogin ? username : "signup"}</a>
-          </li>         
-        </ul>    
-         <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-danger" type="submit">Search</button>
-      </form>            
+            <a class="nav-link text-danger px-3 fw-bolder border border-danger" href="/E-comerce_Website/Pages/signUpPage.html">
+              ${isLogin ? username : "Signup"}
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
   </nav>
-  `
-}
-export default Navbar
+  `;
+};
+
+export default Navbar;
