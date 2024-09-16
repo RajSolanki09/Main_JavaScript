82a23a8e484ef52a5a9beb8041327e9a7c1dfca3
Raj Solanki <147243819+RajSolanki09@users.noreply.github.com>
2024-09-16 10:17:52 -0400

    Exam-9

---
+++ Exam-9_studentData/js/addstudent.js
@@ -0,0 +1,24 @@
+import getvalue from "../components/helper.js";
+import Navbar from "../components/navbar.js";
+
+document.getElementById("navbar").innerHTML = Navbar();
+
+let studentData = JSON.parse(localStorage.getItem('studentData')) || [];
+
+const Studentsubmit = (e) => {
+    e.preventDefault();
+
+    let student = {
+        studentname: getvalue(".name"),
+        course: getvalue(".course"),
+        fees: getvalue(".fees"),
+        studId: getvalue(".studId"),
+    };
+
+    studentData.push(student);
+    console.log(studentData);
+
+    localStorage.setItem("studentData", JSON.stringify(studentData));
+};
+
+document.getElementById("studentDatas").addEventListener("submit", Studentsubmit);
