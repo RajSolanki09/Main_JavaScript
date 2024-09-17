25c904c6a4c25ae63c6935ec4b28351cdc128403
Raj Solanki <147243819+RajSolanki09@users.noreply.github.com>
2024-09-17 09:41:25 +0530

    Delete Exam-9_studentData directory

--- Exam-9_studentData/js/index.js
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-import Navbar from "../components/navbar.js";
-
-document.getElementById("navbar").innerHTML = Navbar()
-=======
-import Navbar from "../components/navbar.js";
-
-document.getElementById("navbar").innerHTML = Navbar()
->>>>>>> c1881dffd2af46068d835ab4cf49bcdbb1fce34a
